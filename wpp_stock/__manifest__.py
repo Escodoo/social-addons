a84660fe5fb5312e953aaf624b0ebf5499ed0e09
Eduardo Lima <eduardolimrib@gmail.com>
2022-05-11 09:49:34 -0300

    [ADD] wpp_stock module

---
+++ wpp_stock/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Wpp Inventory',
+    'summary': """
+        Inventory Implementation for WhatsApp Module""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'depends': ['sale', 'stock', 'wpp'],
+    'data': [
+        'views/template.xml',
+        'views/views.xml',
+    ],
+    'demo': [
+    ],
+}
